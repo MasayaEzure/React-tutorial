e08651289cc6f20ce6ed98e5c0b939e94c4e10ff
MasayaEzure <78490228+MasayaEzure@users.noreply.github.com>
2024-02-11 09:31:02 +0900

    セッション6の学習完了

--- session6/src/App.jsx
+++ session6/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Message } from './components/Message';
 
 export const App = () => {
   const [num, setNum] = useState(0);
+  const [isShowText, setIsShowText] = useState(false);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1);
-    // setNum(num + 1);
   };
+  const onClickToggle = () => {
+    setIsShowText(!isShowText);
+  };
+
+  // なるべく使用しない（useEffectを使わなくて良いケースを考える）
+  useEffect(() => {
+    if (num > 0 && num % 3 === 0) {
+      isShowText || setIsShowText(true);
+    } else {
+      isShowText && setIsShowText(false);
+    }
+  }, [num]);
+
+
   return (
     <>
       <h1 style={{ color: 'red' }}>こんにちは！</h1>
@@ -14,6 +28,8 @@ export const App = () => {
       <Message color="green">元気ですよ！</Message>
       <button onClick={onClickCountUp}>Count Up</button>
       <p>{num}</p>
+      <button onClick={onClickToggle}>show or hide</button>
+      {isShowText && <p>ああああ</p>}
     </>
   );
 };
